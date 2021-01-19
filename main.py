6028b6fdcbc19731a1b341a0b925638cc4fa86e5
Val-erieS <72756972+Val-erieS@users.noreply.github.com>
2021-01-19 09:57:03 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+#while True:
+    #print("sound level" + input.sound_level())
+    #if input.sound_level() > 100:
+        #light.show_animation(light.rainbowAnimation, 500)
+    #else:
+        #light.set_all(light.rgb(0,0,0))
+
+#while True: 
+    #print("light level:" + input.light_level())
+    #if input.light_level() > 15:
+        #music.magic_wand.play()
+    #else: 
+        #music.stop_all_sounds()
+        
 while True:
-    print("sound level" + input.sound_level())
-    if input.sound_level() > 100:
-        light.set_all(light.rgb(23,5,90))
-    else:
-        light.set_all(light.rgb(0,0,0))
+    music.play_melody("E D C D E E D D D E G G E D C D E E E E D D E D C", 120)
+
+    
--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
+// while True:
+// print("sound level" + input.sound_level())
+// if input.sound_level() > 100:
+// light.show_animation(light.rainbowAnimation, 500)
+// else:
+// light.set_all(light.rgb(0,0,0))
+// while True: 
+// print("light level:" + input.light_level())
+// if input.light_level() > 15:
+// music.magic_wand.play()
+// else: 
+// music.stop_all_sounds()
 while (true) {
-    console.log("sound level" + input.soundLevel())
-    if (input.soundLevel() > 100) {
-        light.setAll(light.rgb(23, 5, 90))
-    } else {
-        light.setAll(light.rgb(0, 0, 0))
-    }
-    
+    music.playMelody("E D C D E E D D D E G G E D C D E E E E D D E D C", 120)
 }
